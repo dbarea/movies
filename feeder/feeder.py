8a54629cc35c952d5c42ad706dc333b3888c2231
Seba <scativa@gmail.com>
2022-06-18 00:24:59 -0300

    Feeder toma datos del PLC

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -3,41 +3,27 @@ from time import sleep
 import requests
 from datetime import datetime
 
-
-
-# Python program to demonstrate
-# Conversion of JSON data to
-# dictionary
- 
- 
-# importing the module
-import json
-
 print("Request " + requests.__version__)
 
-
-# jsonf = {"title":"Nueve Reinas","genre":{"_id":"5b21ca3eeb7f6fbccd471818","name":"Action"},"numberInStock":300,"dailyRentalRate":25}
-
-# se agrega un timestam del momento de carga
-now = datetime.now()
-timestamp = datetime.timestamp(now)
-
 while True:
-    rjson = requests.get("http://186.182.76.7:800/awp/datos.json")
+    rjson = requests.get("http://186.182.76.7:800/awp/datos.json") # 186.182.76.7:800 IP PLC de DIAL (NAT del puerto 800)
 
     jsonf = rjson.json()
+    # se agrega un timestam del momento de carga
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
     jsonf['timestamp'] = timestamp
 
     r = requests.post('http://localhost:3002/movie', json = jsonf)
     print(r)
 
-    sleep(10)
+    if r.status_code == 200:
+        dt_obj = datetime.fromtimestamp(jsonf['timestamp'])
+        print(f"json cargado correctamente: {jsonf} at {dt_obj} ({jsonf['timestamp']})")
+    else:
+        print(f"Error en la carga. Status Code: {r.status_code}")
 
-if r.status_code == 200:
-    dt_obj = datetime.fromtimestamp(jsonf['timestamp'])
-    print(f"json cargado correctamente: {jsonf} at {dt_obj} ({jsonf['timestamp']})")
-else:
-    print(f"Error en la carga. Status Code: {r.status_code}")
+    sleep(10)
 
 # r = requests.post('http://localhost:3002/movie', json=jsonf)
 # f"Status Code: {r.status_code}, Response: {r.json()}")
